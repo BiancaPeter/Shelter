0ab9feaf81019a0ca8a42c99b48889de1a7d5321
BiancaPeter <botabiancateodora@gmail.com>
2022-07-16 19:49:11 +0300

    implementare Shelter

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        Dog dog1 = new Dog("Azor",2);
+        Cat cat1 = new Cat("Tom",1);
+        Animal[] animals = new Animal[2];
+        Shelter shelter = new Shelter(animals);
+        shelter.addAnimal(dog1);
+        shelter.addAnimal(cat1);
+        shelter.makeNoise();
+    }
+}
